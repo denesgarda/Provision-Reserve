384d0c7647564319aa0fdcb76ac00ff63beeb2c0
Denes Garda <72540138+denesgarda@users.noreply.github.com>
2025-12-25 00:33:13 -0800

    basic inventory display

--- src/components/main-views/InventoryView.tsx
+++ src/components/main-views/InventoryView.tsx
@@ -1,5 +1,25 @@
+import { useDatabase } from "@/context/DatabaseContext"
+import { Generic } from "@/types";
+
 export function InventoryView() {
+    const databaseService = useDatabase().databaseService;
+
+    const generics = databaseService?.getData().generics;
+
     return (
-        <h1>Invenory</h1>
+        <div>
+            {generics?.map((generic: Generic) => (
+                <div key={generic.id} className="generic-listed">
+                    <p style={{
+                        fontWeight: "700"
+                    }}>{generic.name}</p>
+                    <p style={{
+                        fontSize: "15px",
+                        fontWeight: "500",
+                        color:"rgb(141, 141, 141)"
+                    }}>2 gal • 2 items</p>
+                </div>
+            ))}
+        </div>
     )
 }
